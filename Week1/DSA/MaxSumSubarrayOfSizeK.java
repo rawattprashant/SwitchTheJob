c589acc750f9f99c4832257f52dd274d321f2a1e
rawattprashant <rawatprashant063@gmail.com>
2025-07-17 22:35:55 +0530

    Updated MaxSumSubarrayOfSizeK file

--- Week1/DSA/MaxSumSubarrayOfSizeK.java
+++ Week1/DSA/MaxSumSubarrayOfSizeK.java
@@ -1,2 +1,40 @@
+import java.util.Scanner;
+
 public class MaxSumSubarrayOfSizeK {
+
+    public static void main (String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array size : ");
+        int N = sc.nextInt();
+        System.out.println("Enter window size : ");
+        int K = sc.nextInt();
+        System.out.println("Enter array's element : ");
+        int[] arr = new int[N];
+        for(int i = 0 ; i < N;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("The max Sum of "+K+"-sized sub-array is : " + getMaxSumSubArray(arr, K));
+    }
+
+    private static int getMaxSumSubArray(int[] arr, int k) {
+
+        int start = 0 ;
+        int end = 0 ;
+        int sum = 0 ;
+        int maxSum = 0;
+        while(end < arr.length){
+            sum += arr[end];
+            if((end-start+1 ) < k){
+                end++;
+            } else if ((end-start+1) == k) {
+                maxSum = Math.max(maxSum, sum);
+                sum = sum - arr[start];
+                start++;
+                end++;
+            }
+        }
+        return maxSum;
+    }
 }
